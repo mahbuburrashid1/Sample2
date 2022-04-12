a53d6dda49cc39b9378da18d75d8885337276aa2
Mahbubur Rashid <mahbuburrashidusa@gmail.com>
2022-04-11 23:30:37 -0400

    this is my sample 2 message

---
+++ src/rashid3/LearnPremitive3.java
@@ -0,0 +1,56 @@
+package rashid3;
+
+public class LearnPremitive3 {
+	
+	
+	
+	
+
+	public static void main (String args[]) {
+		int num =40;
+		
+		String firstName="Mahbub";
+		String lastName="Rashid";
+		
+		byte number= -127;
+		short salary = 5011;
+		
+		System.out.println(num); // calling a variable
+		
+		System.out.println ("Mahbub");
+		System.out.println(firstName); //calling a variable
+		System.out.println(lastName);  //calling a variable
+		
+		System.out.println(number);
+		
+		
+		System.out.println(salary);
+		
+		
+	}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
---
+++ src/rashid4/LearnPremitive4.java
@@ -0,0 +1,63 @@
+package rashid4;
+
+public class LearnPremitive4 {
+	
+	
+	
+
+	public static void main (String args[]) {
+		int num =40;
+		
+		String firstName="Mahbub";
+		String lastName="Rashid";
+		
+		byte number= -127;
+		short salary = 5022;
+		
+		System.out.println(num); // calling a variable
+		
+		System.out.println ("Mahbub");
+		System.out.println(firstName); //calling a variable
+		System.out.println(lastName);  //calling a variable
+		
+		System.out.println(number);
+		
+		
+		System.out.println(salary);
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
